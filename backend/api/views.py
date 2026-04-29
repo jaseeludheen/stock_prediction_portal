b129c4487267adcff67cbd4e05eadb9b66500ee2
Jaseeludheen <jaseeljasee1997@gmail.com>
2026-04-29 19:58:52 +0400

    Fetching the data

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from .serializers import StockPredictionSerializer
+from rest_framework.response import Response
+from rest_framework import status
+import yfinance as yf
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from datetime import datetime
+
 
 # Create your views here.
+
+
+class StockPredictionAPIView(APIView):
+    def post(self, request):
+        serialzer = StockPredictionSerializer(data=request.data)
+        if serialzer.is_valid():
+            ticker = serialzer.validated_data['ticker']
+
+            # Fetch the data from yfinance
+            now = datetime.now()
+            start = datetime(now.year-10, now.month, now.day)
+            end = now
+            df = yf.download(ticker, start, end)
+            print(df)
+
+           # check df is empty or not
+            if df.empty:
+                return Response({"error": "No data found for the given ticker.", 
+                                'status': status.HTTP_404_NOT_FOUND})
+            return Response({'status': 'success', 'ticker': ticker})
--- frontend/src/components/dashboard/Dashboard.jsx
+++ frontend/src/components/dashboard/Dashboard.jsx
@@ -1,15 +1,18 @@
 import axios from 'axios'
-import React, {useEffect} from 'react'
+import React, {useEffect, useState} from 'react'
 import axiosInstance from '../../axiosInstance';
 
 
 const Dashboard = () => {
 
+    const [ticker, setTicker] = useState('')
+
+
     useEffect( ()=> {
         const fetchProtectedData = async () => {
             try{
                 const response = await axiosInstance.get('/protected-view/');
-                console.log('Sucess:', response.data);
+                //console.log('Sucess:', response.data);
             }catch(error){
                 console.error('Error fetching data :', error)
             }
@@ -18,10 +21,34 @@ const Dashboard = () => {
         fetchProtectedData()
     },[])
 
+    const handleSubmit = async (e)=> {
+        e.preventDefault();
+        try{
+            const response = await axiosInstance.post('/predict/', {
+                ticker: ticker
+            })
+            
+            console.log(response.data)
+               
+        }catch(error){
+            console.error('There was an error making the API request', error)
+        }
+    }
+
   return (
     <>
-
-    <div className='text-light container'>Dashboard</div>
+        <div className='container'>
+            <div className="row">
+                <div className="col-md-6 mx-auto">
+                    <form onSubmit={handleSubmit}>
+                        <input type="text" className='form-control' placeholder='Enter Stock Ticker' 
+                        onChange={(e) => setTicker(e.target.value)} required
+                        />
+                        <button type="submit" className='btn btn-info mt-3'>See The Prediction</button>
+                    </form>
+                </div>
+            </div>
+        </div>
     
 
 
