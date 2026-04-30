8aea3adb027a382fae89681ed4dd492153aefafe
Jaseeludheen <jaseeljasee1997@gmail.com>
2026-04-30 12:57:47 +0400

    stock prediction portal with ML integration

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,4 +1,7 @@
+from xml.parsers.expat import model
+
 from django.shortcuts import render
+from matplotlib import ticker
 from rest_framework.views import APIView
 from .serializers import StockPredictionSerializer
 from rest_framework.response import Response
@@ -8,6 +11,12 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from datetime import datetime
+import os
+from django.conf import settings
+from .utils import save_plot
+from sklearn.preprocessing import MinMaxScaler
+from keras.models import load_model
+from sklearn.metrics import mean_squared_error, r2_score
 
 
 # Create your views here.
@@ -24,10 +33,139 @@ class StockPredictionAPIView(APIView):
             start = datetime(now.year-10, now.month, now.day)
             end = now
             df = yf.download(ticker, start, end)
-            print(df)
+            #print(df)
 
            # check df is empty or not
             if df.empty:
                 return Response({"error": "No data found for the given ticker.", 
                                 'status': status.HTTP_404_NOT_FOUND})
-            return Response({'status': 'success', 'ticker': ticker})
+            
+            # Reset the index to make 'Date' a column instead of an index
+            df = df.reset_index()
+            #print(df)
+
+            # Generate basic Plot
+            plt.switch_backend('AGG')  #Anti-Grain Geometry - save the plot as an image instead of displaying it, image file store in media folder
+            plt.figure(figsize=(12, 5))
+            plt.plot(df.Close, label='Closing Price')
+            plt.title(f'Closing price of {ticker}')
+            plt.xlabel('Days')
+            plt.ylabel('Close price')
+            plt.legend()
+
+            # save the plot to a file
+            """
+            plot_img_path = f'{ticker}_plot.png'
+            image_path = os.path.join(settings.MEDIA_ROOT, plot_img_path)
+            plt.savefig(image_path)
+            plt.close()
+            plot_img = settings.MEDIA_URL + plot_img_path
+            print(plot_img)
+            """
+            # save the plot to a file
+            plot_img_path = f'{ticker}_plot.png'
+            plot_img = save_plot(plot_img_path)
+
+
+
+            # 100 Days Moving Average
+            ma100 = df.Close.rolling(100).mean()
+            plt.switch_backend('AGG') 
+            plt.figure(figsize=(12, 5))
+            plt.plot(df.Close, label='Closing Price')
+            plt.plot(ma100, 'r', label='100 DMA')
+            plt.title(f'100 Days Moving Average of {ticker}')
+            plt.xlabel('Days')
+            plt.ylabel('Close price')
+            plt.legend()
+
+            # save the plot to a file - MA 100 , dma - days moving average
+            plot_img_path = f'{ticker}_100_dma.png'
+            plot_100_dma = save_plot(plot_img_path)
+
+
+            # 200 Days Moving Average
+            ma200 = df.Close.rolling(200).mean()
+            plt.switch_backend('AGG') 
+            plt.figure(figsize=(12, 5))
+            plt.plot(df.Close, label='Closing Price')
+            plt.plot(ma100, 'r', label='100 DMA')
+            plt.plot(ma200, 'g', label='200 DMA')
+            plt.title(f'200 Days Moving Average of {ticker}')
+            plt.xlabel('Days')
+            plt.ylabel('Close price')
+            plt.legend()
+
+            # save the plot to a file - MA 100 , dma - days moving average
+            plot_img_path = f'{ticker}_200_dma.png'
+            plot_200_dma = save_plot(plot_img_path)
+
+
+            # Splitting Data into Training & Testing Datasets
+            # Data Training
+            data_training = pd.DataFrame(df.Close[0:int(len(df)*0.7)])  # 0-70%
+            # Data Testing
+            data_testing = pd.DataFrame(df.Close[int(len(df)*0.7) : int(len(df))]) # 70-100%
+
+            # Scaling down the data between 0 and 1
+            scaler = MinMaxScaler(feature_range=(0,1)) # fit the data in between 0 and 1.
+
+            # Load ML Model
+            model = load_model('stock_prediction_model.keras')
+
+            # Preparing the test data
+            past_100_days = data_training.tail(100)
+            final_df = pd.concat([past_100_days, data_testing], ignore_index=True)
+            # scale down to 0 & 1
+            input_data = scaler.fit_transform(final_df)
+
+            x_test = []
+            y_test = []
+            for i in range(100, input_data.shape[0]):
+                x_test.append(input_data[i-100: i])
+                y_test.append(input_data[i, 0])
+            x_test, y_test = np.array(x_test), np.array(y_test)
+
+            # Making Predictions
+            y_predicted = model.predict(x_test)  
+
+            # Revert the scaled prices to original price (inverse transform from the scaler) 
+            y_predicted = scaler.inverse_transform(y_predicted.reshape(-1, 1)).flatten()   #  -1 is row , 1 column
+            y_test = scaler.inverse_transform(y_test.reshape(-1, 1)).flatten()
+
+            # Plot the final prediction
+            
+            plt.switch_backend('AGG') 
+            plt.figure(figsize=(12, 5))
+            plt.plot(y_test, 'b', label='Original Price')
+            plt.plot(y_predicted, 'r', label='Predicted Price')
+            plt.title(f'Final Prediction for {ticker}')
+            plt.xlabel('Days')
+            plt.ylabel('Price')
+            plt.legend()
+
+            # save
+            plot_img_path = f'{ticker}_final_prediction.png'
+            plot_prediction = save_plot(plot_img_path)
+
+            # Model Evaluation
+
+            # MSE - Mean Squared Error
+            mse = mean_squared_error(y_test, y_predicted)
+
+            # RMSE - Root Mean Squared Error
+            rmse = np.sqrt(mse)
+
+            # R-squared (R²)
+            r2 = r2_score(y_test, y_predicted)
+
+
+            return Response({'status': 'success', 
+                             'plot_img': plot_img,
+                             'plot_100_dma': plot_100_dma,
+                             'plot_200_dma': plot_200_dma,
+                             'plot_prediction': plot_prediction,
+                             'mse': mse,
+                             'rmse': rmse,
+                             'r2': r2
+                             })
--- frontend/src/components/dashboard/Dashboard.jsx
+++ frontend/src/components/dashboard/Dashboard.jsx
@@ -1,11 +1,25 @@
 import axios from 'axios'
 import React, {useEffect, useState} from 'react'
 import axiosInstance from '../../axiosInstance';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSpinner} from '@fortawesome/free-solid-svg-icons'
 
 
 const Dashboard = () => {
 
     const [ticker, setTicker] = useState('')
+    const [error, setError] = useState()
+    const [loading, setLoading] = useState(false)
+    const [plot, setPlot] = useState()
+    const [ma100, setMA100] = useState()
+    const [ma200, setMA200] = useState()
+    const [prediction, setPrediction] = useState()
+    const [mse, setMSE] = useState()
+    const [rmse, setRMSE] = useState()
+    const [r2, setR2] = useState()
+
+    // import the base URL from the environment
+    const BASE_URL = import.meta.env.VITE_BASE_URL;
 
 
     useEffect( ()=> {
@@ -23,20 +37,42 @@ const Dashboard = () => {
 
     const handleSubmit = async (e)=> {
         e.preventDefault();
+        setLoading(true)
         try{
             const response = await axiosInstance.post('/predict/', {
                 ticker: ticker
             })
-            
+
             console.log(response.data)
+
+            // Set the Plot
+            const plotUrl = `${BASE_URL}${response.data.plot_img}`
+            const ma100Url = `${BASE_URL}${response.data.plot_100_dma}`
+            const ma200Url = `${BASE_URL}${response.data.plot_200_dma}`
+            const predictionUrl = `${BASE_URL}${response.data.plot_prediction}`
+            //console.log(plotUrl)
+
+            setPlot(plotUrl)
+            setMA100(ma100Url)
+            setMA200(ma200Url)
+            setPrediction(predictionUrl)
+
+            setMSE(response.data.mse)
+            setRMSE(response.data.rmse)
+            setR2(response.data.r2)
+
+            if(response.data.error){
+                setError(response.data.error)
+            }
                
         }catch(error){
             console.error('There was an error making the API request', error)
+        }finally{
+            setLoading(false)
         }
     }
 
   return (
-    <>
         <div className='container'>
             <div className="row">
                 <div className="col-md-6 mx-auto">
@@ -44,16 +80,61 @@ const Dashboard = () => {
                         <input type="text" className='form-control' placeholder='Enter Stock Ticker' 
                         onChange={(e) => setTicker(e.target.value)} required
                         />
-                        <button type="submit" className='btn btn-info mt-3'>See The Prediction</button>
+                        <small>{error && <div className="text-danger">{error}</div>}</small>
+                        <div className="d-flex justify-content-center mt-3">
+                            <button type="submit" className='btn btn-info mt-3 '>
+                                {loading ? <span><FontAwesomeIcon icon={faSpinner} spin /> Please wait... </span> : 'See The Prediction'}
+                            </button>
+                        </div>
+                        
                     </form>
                 </div>
+
+                {/* Display the  prediction plots */}
+                { prediction && (
+                    <div className="prediction mt-5">
+                    <div className="p-3">
+                        {plot && (
+                            <img src={plot} alt="" style={ {maxWidth: '100%'} }/>
+                        )}
+                    </div>
+
+                    <div className="p-3">
+                        {ma100 && (
+                            <img src={ma100} alt="" style={ {maxWidth: '100%'} }/>
+                        )}
+                    </div>
+
+                    <div className="p-3">
+                        {ma200 && (
+                            <img src={ma200} alt="" style={ {maxWidth: '100%'} }/>
+                        )}
+                    </div>
+
+                    <div className="p-3">
+                        {prediction && (
+                            <img src={prediction} alt="" style={ {maxWidth: '100%'} }/>
+                        )}
+                    </div>
+
+                    <div className="text-light p-3 bg-dark rounded text-center">
+                        <h4>Model Evaluation Metrics</h4>
+                        <p><strong>Mean Squared Error (MSE):</strong> {mse}</p>
+                        <p><strong>Root Mean Squared Error (RMSE):</strong> {rmse}</p>
+                        <p><strong>R-squared (R²):</strong> {r2}</p>
+                    </div>
+
+                </div>
+                )}
+
+                
+
             </div>
         </div>
     
 
 
-    </>
-    
+
   )
 }
 
